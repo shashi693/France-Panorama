7e30b0fa5731f8d5440b5fad27d89c35a66fa51a
Shashikant Sukenkar <sukenkar@hotmail.com>
2017-12-19 14:01:01 +0530

    France Panorama
    France Panorama

---
+++ app/src/main/java/com/avenueinfotech/francepanorama/MainActivity.java
@@ -0,0 +1,105 @@
+package com.avenueinfotech.francepanorama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaLocation;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleMap.OnMarkerDragListener, StreetViewPanorama.OnStreetViewPanoramaChangeListener {
+
+//    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+//    private static final LatLng SAN_FRAN = new LatLng(37.769263, -122.450727);
+
+    private static final LatLng FRANCE = new LatLng(47.054658, 8.316861);
+
+    private static final String MARKER_POSITION_KEY = "MarkerPosition";
+
+    private Marker mMarker;
+
+    private StreetViewPanorama mStreetViewPanorama;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LatLng markerPosition;
+        if (savedInstanceState == null) {
+            markerPosition = FRANCE;
+        } else {
+            markerPosition = savedInstanceState.getParcelable(MARKER_POSITION_KEY);
+        }
+
+        SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
+                (SupportStreetViewPanoramaFragment)
+                        getSupportFragmentManager().findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(
+                new OnStreetViewPanoramaReadyCallback() {
+                    @Override
+                    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+                        mStreetViewPanorama = panorama;
+                        mStreetViewPanorama.setOnStreetViewPanoramaChangeListener(
+                                MainActivity.this);
+                        // Only set the panorama to SYDNEY on startup (when no panoramas have been
+                        // loaded which is when the savedInstanceState is null).
+                        if (savedInstanceState == null) {
+                            mStreetViewPanorama.setPosition(FRANCE);
+                        }
+                    }
+                });
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                map.setOnMarkerDragListener(MainActivity.this);
+                // Creates a draggable marker. Long press to drag.
+                mMarker = map.addMarker(new MarkerOptions()
+                        .position(markerPosition)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconw))
+                        .draggable(true));
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(MARKER_POSITION_KEY, mMarker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    @Override
+    public void onStreetViewPanoramaChange(StreetViewPanoramaLocation location) {
+        if (location != null) {
+            mMarker.setPosition(location.position);
+        }
+    }
+}
